d7a497e6ef17bb9b5fa43b91d03e65214bb1cb65
lodyne <lodgmtui@gmail.com>
2024-05-10 09:44:44 +0300

    minor change in settings

--- core/settings.py
+++ core/settings.py
@@ -128,7 +128,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-LOGIN_REDIRECT_URL = 'home'
+LOGIN_REDIRECT_URL = 'core:home'
 
 LOGIN_URL = 'login'
 
